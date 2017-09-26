7f992f689be7c2e202e1321f2b9afc437ed559be
Carlos Santos <carlos@one-shift.com>
2017-09-26 12:31:21 +0100

    updates to kiosk mode

--- kiosk/boot_kiosk.bat
+++ kiosk/boot_kiosk.bat
@@ -2,7 +2,7 @@
 taskkill /f /im explorer.exe
 
 :: run kiosk chrome
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" localhost --kiosk --incognito --disable-pinch --overscroll-history-navigation=0
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost/brand-n-play/0/en/bnp-run/ --use-fake-ui-for-media-stream --disable-popup-blocking --kiosk --incognito --disable-pinch --overscroll-history-navigation=0
 
 :: run explorer after shutdown kiosk
 start explorer.exe
